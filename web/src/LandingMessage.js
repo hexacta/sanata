28e936f70fb7e0e3cdac1fca7685fab85ee7e50e
pomber <pombopombopombo@gmail.com>
2017-03-05 20:32:40 -0300

    Change title and landing message

--- web/src/LandingMessage.js
+++ web/src/LandingMessage.js
@@ -4,12 +4,14 @@ class LandingMessage extends Component {
   render() {
     const style = {
       opacity: this.props.motion,
-      height: `${this.props.motion * 30}px`,
-      color: "#555"
+      height: `${this.props.motion * 50}px`,
+      color: "#555",
+      textAlign: "center",
+      maxWidth: "250px"
     };
     return (
       <div style={style}>
-        Enter any twitter username:
+        Enter any twitter username to auto-generate fake tweets: 
       </div>
     );
   }
