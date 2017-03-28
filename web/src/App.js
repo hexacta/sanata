6eec948c1b162ea70b945f7a002d96d3a119f61e
pomber <pombopombopombo@gmail.com>
2017-03-28 16:37:30 -0300

    Fix format

--- web/src/App.js
+++ web/src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
         </HeightMotion>
         <FormMotion loading={this.isLoading} onChange={this.handleLoad} />
         <TweetListBuffer tweets={state.tweets} />
-        <HeightMotion show={this.isLoading} height={30} >
+        <HeightMotion show={this.isLoading} height={30}>
           <StatusBar username={state.username} />
         </HeightMotion>
       </div>
