4e44a58e9a906143e1c6429437354a0419905ede
Anicar Cabrera <acabrera@HEXACTA.COM>
2017-06-13 17:51:27 -0300

    ran styler

--- web/src/Form.js
+++ web/src/Form.js
@@ -27,7 +27,12 @@ class Form extends Component {
     return (
       <div className="form" style={this.formStyle}>
         <Spinner style={this.spinnerStyle} />
-        <TextboxButton onSubmit={this.props.onChange} placeholder="username" bulletpoint="@" style={this.inputStyle} />
+        <TextboxButton
+          onSubmit={this.props.onChange}
+          placeholder="username"
+          prefix="@"
+          style={this.inputStyle}
+        />
       </div>
     );
   }
