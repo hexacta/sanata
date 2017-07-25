ab84f7a9360a3ef74cf5712e5a0909c32e4d8976
Anicar Cabrera <anicarrr@gmail.com>
2017-07-25 19:13:11 -0300

    Remove some tweet.css styles and add glamorous components in tweet.js

--- web/src/Tweet.js
+++ web/src/Tweet.js
@@ -1,6 +1,57 @@
 import React, { Component } from "react";
 import EmbeddedMedia from "./EmbeddedMedia";
+import glamorous from "glamorous";
 import "./Tweet.css";
+
+const MainContainer = glamorous.div({
+  display: "flex",
+  alignItems: "flex-start",
+  justifyContent: "flex-start",
+  flexDirection: "column",
+  width: "80vw",
+  maxWidth: "500px",
+  minHeight: "50px",
+  border: "1px solid #e1e8ed",
+  borderBottom: "0",
+  background: "white",
+  padding: "9px 12px 12px 12px",
+  fontFamily: "Arial, sans-serif",
+  lineHeight: "18px",
+
+  willChange: "opacity, transform",
+  transitionProperty: "opacity, transform",
+  transitionDuration: "0.5s",
+  transitionTimingFunction: "cubic-bezier(0.250, 0.460, 0.450, 0.940)"
+}, prop => prop.mounting && {opacity: "0", transform: "translateY(100%)"});
+
+const TweetMain = glamorous.div({
+  display: "flex",
+  flexDirection: "row"
+});
+
+const TweetAvatar = glamorous.img({
+  width: "48px",
+  minWidth: "48px",
+  height: "48px",
+  borderRadius: "5px",
+  marginRight: "10px"
+});
+
+const TweetBody = glamorous.div({ });
+
+const TweetHeader = glamorous.div({
+  display: "flex",
+  alignItems: "center",
+  flexFlow: "row"
+});
+
+const TweetFullname = glamorous.strong({
+  fontSize: "14px",
+  fontWeight: "bold",
+  color: "#14171a",
+  display: "block",
+  paddingRight: "5px"
+});
 
 class Tweet extends Component {
   state = {
@@ -22,27 +73,24 @@ class Tweet extends Component {
 
   render() {
     const tweet = this.props.tweet;
-    const className = this.state.mounting
-      ? "tweet-container entrance"
-      : "tweet-container";
     return (
-      <div className={className}>
-        <div className="tweet-main">
-          <img className="tweet-avatar" src={tweet.avatar} alt="avatar" />
-          <div className="tweet">
-            <div className="tweet-header">
-              <strong className="tweet-fullname">{tweet.fullname}</strong>
+      <MainContainer mounting={this.state.mounting}>
+        <TweetMain>
+          <TweetAvatar src={tweet.avatar} alt="avatar" />
+          <TweetBody>
+            <TweetHeader>
+              <TweetFullname>{tweet.fullname}</TweetFullname>
               <span className="tweet-username">
                 <span className="fake">@fake</span>{tweet.username}
               </span>
-            </div>
+            </TweetHeader>
             <span className="tweet-text">{tweet.text}</span>
             <div className="tweet-media-preview">
               <EmbeddedMedia targetUrl={this.matchUrl(tweet.text)} />
             </div>
-          </div>
-        </div>
-      </div>
+          </TweetBody>
+        </TweetMain>
+      </MainContainer>
     );
   }
 }
