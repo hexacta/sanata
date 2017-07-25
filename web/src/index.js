f6807d2581a153aa47725f90e81639794b6b79fa
Anicar Cabrera <anicarrr@gmail.com>
2017-07-25 16:58:38 -0300

    delete index.css and add glamorous component in index.js

--- web/src/index.js
+++ web/src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import glamorous from "glamorous";
+
+const Body = glamorous.body({
+  margin: "0",
+  padding: "0",
+  fontFamily: "sans-serif"
+});
 
 ReactDOM.render(
-  <Router>
-    <Route path="/:username?" component={App} />
-  </Router>,
+  <Body>
+	<Router>
+	  <Route path="/:username?" component={App} />
+	</Router>
+  </Body>,
   document.getElementById("root")
 );
